7407f507ec0dc396d255bb24eb484d33df830e6b
Michal Drla <michal.drla@outlook.com>
2025-06-04 19:08:22 +0200

    feat: Add workspaces
    Signed-off-by: Michal Drla <michal.drla@outlook.com>

--- app.ts
+++ app.ts
@@ -5,22 +5,22 @@ import style from './style.scss';
 import Bar from './widget/Bar';
 function main() {
     const hyprland = Hyprland.get_default();
-    
-    const bars = new Map<Gdk.Monitor, Gtk.Widget>()
+
+    const bars = new Map<Gdk.Monitor, Gtk.Widget>();
 
     // initialize
-    for (const gdkmonitor of App.get_monitors()) {
-        bars.set(gdkmonitor, Bar(gdkmonitor, hyprland))
-    }
+    App.get_monitors().forEach((gdkmonitor, idx) => {
+        bars.set(gdkmonitor, Bar(gdkmonitor, hyprland));
+    });
 
-    App.connect("monitor-added", (_, gdkmonitor) => {
-        bars.set(gdkmonitor, Bar(gdkmonitor, hyprland))
-    })
+    App.connect('monitor-added', (_, gdkmonitor) => {
+        bars.set(gdkmonitor, Bar(gdkmonitor, hyprland));
+    });
 
-    App.connect("monitor-removed", (_, gdkmonitor) => {
-        bars.get(gdkmonitor)?.destroy()
-        bars.delete(gdkmonitor)
-    })
+    App.connect('monitor-removed', (_, gdkmonitor) => {
+        bars.get(gdkmonitor)?.destroy();
+        bars.delete(gdkmonitor);
+    });
 }
 App.start({
     css: style,
--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -16,12 +16,12 @@ export default function Bar(monitor: Gdk.Monitor, hyprland: Hyprland) {
         application={App}>
         <centerbox cssName="centerbox">
             <box halign={Gtk.Align.START}>
-                {hyprland.get_workspaces().reverse().map(workspace =>(
+                {hyprland.get_workspaces().filter(workspace => workspace.get_monitor().get_name() == monitor.get_connector()).sort((workspace_1, workspace_2) => workspace_1.get_name() - workspace_2.get_name()).map(workspace =>(
                 <button
                         onClicked={() => workspace.focus()}
                         label={workspace.get_name()}
                 />
-                
+
                 ))}
             </box>
 
