8b7de1ffd01c9852e57c388f0eece545c749e8da
Michal Drla <michal.drla@outlook.com>
2025-06-05 17:30:20 +0200

    feat: Extract leftBar
    Signed-off-by: Michal Drla <michal.drla@outlook.com>

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -1,47 +1,65 @@
-import { App, Astal, Gtk, Gdk } from "astal/gtk4"
+import { App, Astal, Gtk, Gdk } from 'astal/gtk4';
 import Hyprland from 'gi://AstalHyprland';
-import { Variable } from "astal"
+import { Variable } from 'astal';
 
-const time = Variable("").poll(1000, "date")
+const time = Variable('').poll(1000, 'date');
+type Props = {
+    hyprland: Hyprland;
+    monitor: Gdk.Monitor;
+}
 
-export default function Bar(monitor: Gdk.Monitor, hyprland: Hyprland) {
-    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
-
-    return <window
-        visible
-        cssClasses={["Bar"]}
-        gdkmonitor={monitor}
-        exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={TOP | LEFT | RIGHT}
-        application={App}>
-        <centerbox cssName="centerbox">
-            <box halign={Gtk.Align.START}>
-                {hyprland.get_workspaces().filter(workspace => workspace.get_monitor().get_name() == monitor.get_connector()).sort((workspace_1, workspace_2) => workspace_1.get_name() - workspace_2.get_name()).map(workspace =>(
-                <button
+function LeftBar({monitor, hyprland}: Props): JSX.Element {
+    return (
+        <box halign={Gtk.Align.START}>
+            {hyprland
+                .get_workspaces()
+                .filter(
+                    (workspace) =>
+                        workspace.get_monitor().get_name() ==
+                        monitor.get_connector()
+                )
+                .sort(
+                    (workspace_1, workspace_2) =>
+                        workspace_1.get_name() - workspace_2.get_name()
+                )
+                .map((workspace) => (
+                    <button
                         onClicked={() => workspace.focus()}
                         label={workspace.get_name()}
-                />
-
+                    />
                 ))}
-            </box>
+        </box>
+    );
+}
+export default function Bar(monitor: Gdk.Monitor, hyprland: Hyprland) {
+    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor;
 
-            <button
-                onClicked="echo hello"
-                hexpand
-                halign={Gtk.Align.CENTER}
-            >
-                Welcome to AGS!
-            </button>
-            <box />
-            <menubutton
-                hexpand
-                halign={Gtk.Align.CENTER}
-            >
-                <label label={time()} />
-                <popover>
-                    <Gtk.Calendar />
-                </popover>
-            </menubutton>
-        </centerbox>
-    </window>
+    return (
+        <window
+            visible
+            cssClasses={['Bar']}
+            gdkmonitor={monitor}
+            exclusivity={Astal.Exclusivity.EXCLUSIVE}
+            anchor={TOP | LEFT | RIGHT}
+            application={App}
+        >
+            <centerbox cssName="centerbox">
+                <LeftBar monitor={monitor} hyprland={hyprland} />
+                <button
+                    onClicked="echo hello"
+                    hexpand
+                    halign={Gtk.Align.CENTER}
+                >
+                    Welcome to AGS!
+                </button>
+                <box />
+                <menubutton hexpand halign={Gtk.Align.CENTER}>
+                    <label label={time()} />
+                    <popover>
+                        <Gtk.Calendar />
+                    </popover>
+                </menubutton>
+            </centerbox>
+        </window>
+    );
 }
