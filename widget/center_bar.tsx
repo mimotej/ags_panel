fa3a67de367837cf119663713f542c1ff78cdb6b
Michal Drla <michal.drla@outlook.com>
2025-06-29 17:44:27 +0200

    feat: Add initial version of media widget
    Signed-off-by: Michal Drla <michal.drla@outlook.com>

--- widget/center_bar.tsx
+++ widget/center_bar.tsx
@@ -1,14 +1,11 @@
-import { Props } from './Bar';
 import { bind } from 'astal';
-import Hyprland from 'gi://AstalHyprland';
-import Workspace from 'gi://AstalHyprland';
-import { Variable } from 'astal';
 import Mpris from 'gi://AstalMpris';
-export function CenterBar({ monitor, hyprland }: Props): JSX.Element {
+export function CenterBar(): JSX.Element {
     const youtube_music = Mpris.Player.new('YoutubeMusic');
     return (
         <box>
         <button>
+
             <box>
                 <image file={bind(youtube_music, "coverArt")} />
                 <label label={bind(youtube_music, "artist")} />
@@ -16,13 +13,20 @@ export function CenterBar({ monitor, hyprland }: Props): JSX.Element {
                 <label label={bind(youtube_music, "title")} />
             </box>
         </button>
-        <button>
+        <button onClicked={() => youtube_music.previous()}>
+            <image iconName={"media-seek-backward"} />
+        </button>
+        <button onClicked={() => youtube_music.play_pause()}>
             <image iconName={bind(youtube_music, "playbackStatus").as(status =>
             status === Mpris.PlaybackStatus.PLAYING
               ? "media-playback-pause"
               : "media-playback-start"
           )} />
         </button>
+        <button onClicked={() => youtube_music.next()}>
+            <image iconName={"media-seek-forward"} />
+        </button>
+
         </box>
     );
 }
